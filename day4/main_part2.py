84efc7dbd85511505e1dc481051335e557b0f27a
kzabashta <kosta.zabashta@gmail.com>
2017-12-04 18:19:21 -0500

    Day 4

--- day4/main_part2.py
+++ day4/main_part2.py
@@ -8,11 +8,15 @@ for line in contents:
     unique = set()
     no_dupes = True
     for word in words:
-        if not word in unique and :
-            unique.add(word)
+        word_char_arr = list(word)
+        word_char_arr.sort()
+        normalized_word = ''.join(word_char_arr)
+        if not normalized_word in unique:
+            unique.add(normalized_word)
         else:
             no_dupes = False
             break
+
     if no_dupes:
         total += 1
 
