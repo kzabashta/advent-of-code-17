c7080123459cd8672329dcf2dd04ba95e8e04a9f
kzabashta <kosta.zabashta@gmail.com>
2017-12-06 11:34:29 -0500

    Day 6

---
+++ day6/main.py
@@ -0,0 +1,32 @@
+#!/usr/bin/env python
+
+f = open('/Users/kosta/dev/advent-of-code-17/day6/input.txt')
+banks = list(map(int, f.readline().split()))
+
+i = 0
+loops = 1
+patterns = set()
+circuit_breaker = False
+
+while(True):
+    largest_index = 0
+    largest = banks[0]
+    # find largest index
+    for idx, bank in enumerate(banks):
+        if bank > largest:
+            largest = bank
+            largest_index = idx
+    # re-distribute
+    j = largest_index + 1
+    banks[largest_index] = 0
+    while (largest > 0):
+        if j > len(banks) - 1:
+            j = 0
+        banks[j] += 1
+        largest -= 1
+        j+=1
+    if tuple(banks) in patterns:
+        break
+    patterns.add(tuple(banks))
+    loops += 1
+print(loops)
