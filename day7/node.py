de35b9caba5bdd2c19cf0635585451b4900cf539
kzabashta <kosta.zabashta@gmail.com>
2017-12-07 20:39:40 -0500

    oops

--- day7/node.py
+++ day7/node.py
@@ -1,9 +1,5 @@
 class Node:
     
-    def __init__(self):
-        self.weight = 0.0
-        self.children = []
-
     def __init__(self, weight, children):
         self.weight = weight
         self.children = children
