37ed6231ef9e47113cf079ab67b9890ffb26513a
kzabashta <kosta.zabashta@gmail.com>
2017-12-08 23:38:38 -0500

    so far

---
+++ day8/main.py
@@ -0,0 +1,35 @@
+#!/usr/bin/env python
+
+import re
+
+f = open('/Users/kosta/dev/advent-of-code-17/day8/input.txt')
+contents = f.readlines()
+
+registers = {}
+
+for line in contents:
+    struct = re.findall(
+        '([a-z])\s(inc|dec)\s(-?\d+)\sif\s([a-z])\s(.*)\s(-?\d+)', line)[0]
+
+    register = struct[0]
+    operand = struct[1]
+    change = int(struct[2])
+    cond_register = struct[3]
+    cond_operand = struct[4]
+    cond_val = int(struct[5])
+
+    if not cond_register in registers:
+        registers[cond_register] = 0
+
+    if not register in registers:
+        registers[register] = 0
+
+    condition = eval('registers[\'%s\'] %s %i' %
+                     (cond_register, cond_operand, cond_val))
+
+    if condition:
+        registers[register] += change if operand == 'inc' else -change
+
+    maximum_register = max(registers, key=registers.get)
+
+print(registers[maximum_register])
