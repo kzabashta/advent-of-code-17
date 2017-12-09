c77ae6eb029b12650726f153b52d967ed9d3b7b1
kzabashta <kosta.zabashta@gmail.com>
2017-12-09 12:02:55 -0500

    Day 9 part 2

---
+++ day9/main_part2.py
@@ -0,0 +1,52 @@
+#!/usr/bin/env python
+
+class SimpleStack:
+    data = []
+
+    def __init__(self):
+        pass
+    
+    def push(self, item):
+        self.data.append(item)
+
+    def pop(self):
+        last = self.data[len(self.data) - 1]
+        self.data = self.data[:-1]
+        return last
+
+    def is_empty(self):
+        return len(self.data) == 0
+
+    def peek(self):
+        return self.data[len(self.data) - 1]
+
+if __name__ == '__main__':
+    f = open('/Users/kosta/dev/advent-of-code-17/day9/input.txt')
+    line = f.readline()
+    cumulative = 0
+    s = SimpleStack()
+    s.push(0)
+    
+    is_garbage = False
+    is_canceling = False
+
+    for char in line:
+        if is_canceling:
+            is_canceling = False
+            continue
+
+        if char == '!':
+            is_canceling = True
+            continue
+
+        if char == '>':
+            is_garbage = False
+            continue
+        if char == '<' and not is_garbage:
+            is_garbage = True
+            continue
+
+        if is_garbage:
+            cumulative += 1
+
+    print(cumulative)
