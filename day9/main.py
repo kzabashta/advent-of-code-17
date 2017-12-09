c1d7455e96773cdd36926cabc4eb40d093e94cd2
kzabashta <kosta.zabashta@gmail.com>
2017-12-09 11:46:21 -0500

    part 1

--- day9/main.py
+++ day9/main.py
@@ -1,26 +1,53 @@
 #!/usr/bin/env python
 
-class SimpleQueue:
+class SimpleStack:
     data = []
 
     def __init__(self):
         pass
     
-    def enqueue(self, item):
+    def push(self, item):
         self.data.append(item)
 
-    def dequeue(self):
-        head = self.data[0]
-        self.data = self.data[1:]
-        print(self.data)
-        return head
+    def pop(self):
+        last = self.data[len(self.data) - 1]
+        self.data = self.data[:-1]
+        return last
 
     def is_empty(self):
         return len(self.data) == 0
 
     def peek(self):
-        return self.data[0]
+        return self.data[len(self.data) - 1]
 
 if __name__ == '__main__':
     f = open('/Users/kosta/dev/advent-of-code-17/day9/input.txt')
     line = f.readline()
+    cumulative = 0
+    s = SimpleStack()
+    s.push(0)
+    
+    is_garbage = False
+    is_canceling = False
+
+    for char in line:
+        if is_canceling:
+            is_canceling = False
+            continue
+
+        if char == '!':
+            is_canceling = True
+            continue
+
+        if char == '>':
+            is_garbage = False
+        if char == '<':
+            is_garbage = True
+
+        if not is_garbage:
+            if char == '{':
+                s.push(s.peek()+1)
+            if char == '}':
+                cumulative += s.pop()
+
+    print(cumulative)
