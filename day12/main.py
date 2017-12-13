edce5857dfac7f01fe3ae265a8ae44e3b50376c8
kzabashta <kosta.zabashta@gmail.com>
2017-12-12 19:21:57 -0500

    let's see if it works

--- day12/main.py
+++ day12/main.py
@@ -5,8 +5,26 @@ import re
 f = open('/Users/kosta/dev/advent-of-code-17/day12/input.txt')
 links = f.readlines()
 graph = {}
-    
+
+def traverse_graph(node):
+    node['is_visited'] = True
+    for edge in node['edges']:
+        if graph[edge] == 0:
+            return True
+        if not graph[edge]['is_visited']:
+            traverse_graph(graph[edge])
+    return False
+
 for link in links:
     edges = re.findall('(\d+)\s<->\s(.*)', link)[0]
     node = edges[0]
     edges = list(map(int, edges[1].split(',')))
+    graph[node] = {'is_visited': False, 'edges': edges}
+
+def clear_graph(graph):
+    for key in graph:
+        graph[key]['is_visited'] = False
+
+for node in graph:
+    print(traverse_graph(graph[node]))
+    clear_graph(graph)
