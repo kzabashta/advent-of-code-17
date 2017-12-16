af0d9121e598d6df3ee67ce5735442c8fe096a97
kzabashta <kosta.zabashta@gmail.com>
2017-12-16 11:54:17 -0500

    think finished part 1?

--- day16/main.py
+++ day16/main.py
@@ -3,46 +3,55 @@
 import re
 import string
 
-def handle_spin(x):
-    pass
+SPIN_REGEX     = r's(\d+)'
+EXCHANGE_REGEX = r'x(\d+)/(\d+)'
+PARTNER_REGEX  = r'p(\w+)/(\w+)'
 
-def handle_exchange(a, b):
-    pass
+def handle_spin(move, progs):
+    r = re.search(SPIN_REGEX, move)
+    spin_by = int(r.group(1))
+    spun = progs[-spin_by:]
+    spun.extend(progs[:len(progs)-spin_by])
+    return spun
 
-def handle_partner(a, b):
-    pass
+def handle_exchange(move, progs):
+    r = re.search(EXCHANGE_REGEX, move)
+    a = int(r.group(1))
+    b = int(r.group(2))
+
+    temp = progs[a]
+    progs[a] = progs[b]
+    progs[b] = temp
+
+    return progs
+
+def handle_partner(move, progs):
+    r = re.search(PARTNER_REGEX, move)
+    a = r.group(1)
+    b = r.group(2)
+
+    a = progs.index(a)
+    b = progs.index(b)
+
+    temp = progs[a]
+    progs[a] = progs[b]
+    progs[b] = temp
+
+    return progs
 
 def dance(progs, moves):
     for move in moves:
         if move[0] == 's':
-             progs = handle_spin()
+            progs = handle_spin(move, progs)
+        elif move[0] == 'x':
+            progs = handle_exchange(move, progs)
+        elif move[0] == 'p':
+            progs = handle_partner(move, progs)
+    return progs
 
 if __name__ == '__main__':
     progs = list(string.ascii_lowercase)[:16]
-    f = open('/Users/kosta/dev/advent-of-code-17/day16/input.txt')
+    f = open('/Users/kosta/dev/advent-of-code-17/day16/input.txt', 'r')
     moves = f.read().split(',')
-
-
-
-"""layers = {}
-total_layers = 0
-
-for line in lines:
-    parsed_layer = re.findall('(\d+):\s(\d+)', line)[0]
-    layers[int(parsed_layer[0])] = {'current': 0, 'dir': 1, 'max': int(parsed_layer[1])}
-    total_layers = int(parsed_layer[0])
-
-severity = 0
-
-for i in range(0, total_layers+1):
-    if i in layers and layers[i]['current'] == 0:
-        severity += i * layers[i]['max']
-    for depth in layers:
-        if layers[depth]['current'] == layers[depth]['max'] - 1:
-            layers[depth]['dir'] = -1
-        elif layers[depth]['current'] == 0:
-            layers[depth]['dir'] = 1
-        layers[depth]['current'] += layers[depth]['dir']
-
-print(severity)
-"""
+    f.close()
+    print(''.join(dance(progs, moves)))
