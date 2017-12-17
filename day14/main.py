0e87bea4b78676f27b88247c70e0fd6382127154
kzabashta <kosta.zabashta@gmail.com>
2017-12-16 22:38:10 -0500

    Day 14

--- day14/main.py
+++ day14/main.py
@@ -3,5 +3,16 @@
 from knot_hash import knot_hash
 
 INPUT = 'nbysizxe'
+NUM_BITS = 4
 
-print(knot_hash(INPUT))
+hashes = ['%s-%d' % (INPUT, x) for x in range(0, 128)]
+total = 0
+
+for hash in hashes:
+    knot_hash_output = knot_hash(hash)
+
+    # from https://stackoverflow.com/questions/1425493/convert-hex-to-binary
+    binary = ''.join([bin(int(x, 16))[2:].zfill(NUM_BITS) for x in knot_hash_output])
+    total += binary.count('1')
+
+print(total)
